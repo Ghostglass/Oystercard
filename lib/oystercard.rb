ad87dd433f30879f6be2d31320ba648ada043794
Kimberly Thach <64843479+Ghostglass@users.noreply.github.com>
2020-12-16 13:49:35 +0000

    OysterCard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,3 +8,15 @@ class Oystercard
     end
 end
 
+class Oystercard
+    attr_reader :balance
+
+    def initialize
+        @balance = 0 
+    end
+end
+
+    def top_up(amount)
+        @balance = balance + amount
+    end
+
